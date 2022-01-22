a1d808930865529447e575029cebccf8f2d7aba0
Christy Crook <ccrook3@gmail.com>
2022-01-22 09:46:22 -0600

    added node modules

---
+++ app.js
@@ -0,0 +1,15 @@
+let songs = [{
+    name: "Happiness Begins",
+    publishDate: "2019",
+    imgUrl: "images/happinessBegins.jpg",
+
+    songTitles:["Sucker", "Only Human", "I Believe", "Love Her"],
+    },
+    {
+    name: "A Little Bit Longer",
+    publishDate: "2008",
+    imgUrl: "images/aLittleBitLonger.jpg",
+
+    songTitles: ["Burnin' Up", "Lovebug", "Tonight", "A Little Bit Longer"],
+    }
+];
